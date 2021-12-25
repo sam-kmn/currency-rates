769387f7bb73bddeff8ce45a0f1a68ada8f1f6d0
sam-kmn <sancho.kmn@gmail.com>
2021-12-25 19:30:00 +0100

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Rates from "./routes/Rates";
 import Available from "./routes/Available";
 import Error from "./routes/Error";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Rates />} />
         <Route path="/available" element={<Available />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to={"/"}>
+          Currency Rates
+        </Link>
+
+        <Link
+          className="navbar-item text-decoration-none text-light"
+          to={"/available"}
+        >
+          Available currencies
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
